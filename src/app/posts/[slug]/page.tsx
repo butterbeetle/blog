3be847758dde410f68ebf984c8d165bba05944e9
butterbeetle <butterbeetle@naver.com>
2023-07-19 21:38:09 +0900

    Style : Markdown Viewer

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostData } from "@/service/posts";
 
 type Props = {
@@ -11,7 +12,7 @@ export default async function PostPage({ params: { slug } }: Props) {
   return (
     <>
       <h1>{post.title}</h1>
-      <p>{post.content}</p>
+      <MarkdownViewer content={post.content} />
     </>
   );
 }
