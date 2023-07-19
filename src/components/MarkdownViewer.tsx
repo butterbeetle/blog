518d6c2d4d6afe55f52cfdca69824dea71ba60b8
butterbeetle <butterbeetle@naver.com>
2023-07-19 22:12:39 +0900

    Style : Markdown img, syntax 적용

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,9 +1,44 @@
+"use client";
+import Image from "next/image";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose max-w-none"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={dark}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-60 object-cover"
+            src={image.src || " "}
+            alt={image.alt || " "}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
