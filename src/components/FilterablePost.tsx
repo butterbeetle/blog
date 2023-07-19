cf5c8bf6341a6e7ed121d366e782d18a413e221f
butterbeetle <butterbeetle@naver.com>
2023-07-19 20:11:48 +0900

    Feat : 포스트 페이지
    필터기능

---
+++ src/components/FilterablePost.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Post, getAllPosts } from "@/service/posts";
+import { useState } from "react";
+import PostsGrid from "./PostsGrid";
+import Categories from "./Categories";
+
+type Props = {
+  posts: Post[];
+  categoires: string[];
+};
+const ALL_POSTS = "All Posts";
+
+export default function FilterablePosts({ posts, categoires }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+  return (
+    <section>
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categoires]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+}
