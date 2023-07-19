b69fbb0c43efe5095db2c0342b9e95dc5e14b340
butterbeetle <butterbeetle@naver.com>
2023-07-19 17:41:49 +0900

    Style : Card 스타일링

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -4,7 +4,7 @@ import PostCard from "./PostCard";
 type Props = { posts: Post[] };
 export default function PostsGrid({ posts }: Props) {
   return (
-    <ul>
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {posts.map((post) => (
         <li key={post.path}>
           <PostCard post={post} />
