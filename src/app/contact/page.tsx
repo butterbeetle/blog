cbbb52f72963f4558386e61d7975eb013eb4ed9a
butterbeetle <butterbeetle@naver.com>
2023-07-19 23:25:22 +0900

    Feat : Contact Page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,28 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub } from "@react-icons/all-files/ai/AiFillGithub";
+
+const LINKS = [{ icon: <AiFillGithub />, url: "" }];
+
 export default function ContactPage() {
-  return <p>연락하기</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>butterbeetle@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
